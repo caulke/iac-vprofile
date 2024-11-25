96e953a135ead7ddde279a355d3aa1a91a3ae5e5
caulke <kelvin.caulker.tripticom@gmail.com>
2024-11-25 19:31:26 +0100

    stages

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -28,7 +28,7 @@ terraform {
 
  backend "s3" {
     bucket         = "pamy-s3-bucket"
-    key            = "terraform/state"
+    key            = "env/state"
     region         = "us-east-2"
     access_key     = var.AWS_ACCESS_KEY_ID
     secret_key     = var.AWS_SECRET_ACCESS_KEY
