e983d51aa68ec7f8c34c4933337bf38639a78bcf
caulke <kelvin.caulker.tripticom@gmail.com>
2024-11-26 15:52:54 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "kels-eks"
+  default     = "vprofile-eks"
 }
 
 ####
